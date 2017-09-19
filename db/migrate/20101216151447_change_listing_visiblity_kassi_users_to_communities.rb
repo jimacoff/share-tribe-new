d1137f80b628985c7176129a4ae6c8536b84034f
Savan Raval <savan_raval@yahoo.com>
2017-09-19 17:06:13 +0530

    Text

--- db/migrate/20101216151447_change_listing_visiblity_kassi_users_to_communities.rb
+++ db/migrate/20101216151447_change_listing_visiblity_kassi_users_to_communities.rb
@@ -1,9 +1,11 @@
 class ChangeListingVisiblityKassiUsersToCommunities < ActiveRecord::Migration[5.1]
   def self.up
-    Listing.update_all("visibility = 'communities'", "visibility LIKE 'kassi_users'")
+    # Listing.update_all("visibility = 'communities'", "visibility LIKE 'kassi_users'")
+    Listing.update_all(visibility: 'communities')
   end
 
   def self.down
-    Listing.update_all("visibility = 'kassi_users'", "visibility LIKE 'communities'")
+    Listing.update_all(visibility: 'kassi_users')
+    # Listing.update_all("visibility = 'kassi_users'", "visibility LIKE 'communities'")
   end
 end
