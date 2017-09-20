e0d705902ab2a6872290fbf3264612a21b61239a
Savan Raval <savan_raval@yahoo.com>
2017-09-20 18:01:19 +0530

    Migration updated

--- db/migrate/20130328155825_add_payment_to_community_categories.rb
+++ db/migrate/20130328155825_add_payment_to_community_categories.rb
@@ -1,6 +1,6 @@
 class AddPaymentToCommunityCategories < ActiveRecord::Migration[4.2]
   def up
-    add_column :community_categories, :payment, :boolean, :default => false
+    # add_column :community_categories, :payment, :boolean, :default => false
     # CategoriesHelper.add_custom_price_quantity_placeholders
   end
   
